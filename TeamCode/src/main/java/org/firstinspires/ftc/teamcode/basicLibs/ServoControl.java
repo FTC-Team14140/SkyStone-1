d87aba4513e59a187ed87b95b569927dbc91da1a
rusla <Ruslanafogler@gmail.com>
2020-07-09 15:44:51 -0700

    Added a reasonable speed parameter to ServoControl, also made other modifications in case of errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicLibs/ServoControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicLibs/ServoControl.java
@@ -6,40 +6,53 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ServoControl {
 
     Servo servo;
-    double maxAngularVelocity;
+    final double RANGE_OF_SERVO_IN_DEGREES = 180;
 
-    public ServoControl(Servo servo, double maxAngularVelocity){
+    double maxUnloadSpeedFor60deg; //0.16 sec to travel 60 degrees(taken off of servocity website spec styles)
+    double maxDegreesPerSec; //60 deg/0.16 sec = 375 degrees per sec
+
+    public ServoControl(Servo servo, double maxUnloadSpeedFor60deg) {
         this.servo = servo;
-        this.maxAngularVelocity = maxAngularVelocity;
+        this.maxUnloadSpeedFor60deg = maxUnloadSpeedFor60deg;
+        maxDegreesPerSec = 60 / maxUnloadSpeedFor60deg;
     }
 
-    public void runToPosition(double angularVelocity, double targetPosition){
-        if(angularVelocity > maxAngularVelocity){
-            //log error for attempting to move too fast
+    public void runToPosition(double degreesPerSec, double targetPosition) {
+        if (degreesPerSec > maxDegreesPerSec) {
+            //log speed to high :C
+            return;
+        } else if(targetPosition > Servo.MAX_POSITION || targetPosition < Servo.MIN_POSITION){
+            //log provided invalid position
             return;
         }
-
-        double distance = targetPosition - servo.getPosition();
-        double totalTime = distance/angularVelocity;
-        double newPosition;
+        double initialPosition = servo.getPosition(); //number from 0.0 to 1.0
+        double distance = targetPosition - initialPosition;
+        boolean positiveDirection = (distance > 0 ? true : false);
+        double totalTime = Math.abs(((distance * RANGE_OF_SERVO_IN_DEGREES) / degreesPerSec) * 1000); //in milliseconds, hence the 1000
+        double changeInPosition;
         long startTime = System.currentTimeMillis();
         long now = System.currentTimeMillis();
 
-        while(now < startTime + totalTime){
-            newPosition = angularVelocity * (now - startTime);
-            servo.setPosition(newPosition);
+        while (now < startTime + totalTime) {
+            changeInPosition = ((degreesPerSec * (now - startTime)/1000) / RANGE_OF_SERVO_IN_DEGREES);
+            if(positiveDirection){
+                servo.setPosition(initialPosition + changeInPosition);
+            } else {
+                servo.setPosition(initialPosition - changeInPosition);
+            }
             now = System.currentTimeMillis();
         }
 
     }
 
-    public void runToPositionNoWait(final double angularVelocity, final double targetPosition){
-        Thread thread = new Thread(new Runnable(){
-            public void run(){
-                runToPosition(angularVelocity, targetPosition);
+    public void runToPositionNoWait(final double degreesPerSec, final double targetPosition) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                runToPosition(degreesPerSec, targetPosition);
             }
         });
         thread.start();
     }
-
 }
+
+
